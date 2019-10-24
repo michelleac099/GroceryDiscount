d07bc4f5b8135f440506c47ae73ffd9def8b05c1
Michelle Chis <s1230292@monmouth.edu>
2019-10-23 20:12:46 -0400

    Need to figure out how to verify for number input.

--- GroceryDiscountSecondVersion/src/GroceryDiscountSecondVersion.java
+++ GroceryDiscountSecondVersion/src/GroceryDiscountSecondVersion.java
@@ -2,31 +2,18 @@ import java.util.Scanner;
 public class GroceryDiscountSecondVersion {
 
 	public static void main(String[] args) {
-		
-		/*Coupon discounts:
-		 * 
-		 * Less than $10: No discount
-		 * $10 to $60 : 8%
-		 * More than $60 to $150: 10%
-		 * More than $150 to $210: 12%
-		 * More than $210: 14%
-		 * 
-		 * "You win a discount of $amount (x% of your purchase)"
-		 */
 		 
 		Scanner in = new Scanner(System.in);
 		double moneySpent = 1;
-		
+	
 		while (moneySpent != 0) {
-		System.out.print("Please enter the cost of your groceries, or type '0' to quit: ");
-		moneySpent = in.nextDouble();
-		
-		//How to validate that moneySpent is a number?
-		
-		while (moneySpent>1000) {
-			System.out.print("Please enter a cost that is less than $1,000, or type '0' to quit: ");
+			System.out.print("Enter the cost of your groceries, or type '0' to quit: ");
 			moneySpent = in.nextDouble();
-		} //Validating that the cost isn't bigger than 1000
+		
+			while (moneySpent>1000) {
+				System.out.print("Please enter a cost that is less than $1,000, or type '0' to quit: ");
+				moneySpent = in.nextDouble();
+			} //Validating that the cost isn't bigger than 1000
 				
 		if (moneySpent == 0) {
 			break;
@@ -39,31 +26,31 @@ public class GroceryDiscountSecondVersion {
 		
 		if (moneySpent < 10) {
 			System.out.println("You are not eligible for a coupon.");
-		}
+		} // spending less than 10
 		else if (moneySpent <= 60) {
 			System.out.print("You earn a discount coupon of $");
 			System.out.printf("%.2f", eightPercentDiscount);
 			System.out.print(". (8% of your purchase)");
 			System.out.println(" ");
-		}
+		} // if spending 60 or more
 		else if (moneySpent <= 150) {
 			System.out.print("You earn a discount coupon of $");
 			System.out.printf("%.2f", tenPercentDiscount);
 			System.out.print(". (10% of your purchase)");
 			System.out.println(" ");
-		}
+		} // if spending 150 or more
 		else if (moneySpent <= 210) {
 			System.out.print("You earn a discount coupon of $");
 			System.out.printf("%.2f", twelvePercentDiscount);
 			System.out.print(". (12% of your purchase)");
 			System.out.println(" ");
-		}
+		} // if spending 210 or more
 		else if (moneySpent > 210) {
 			System.out.print("You earn a discount coupon of $");
 			System.out.printf("%.2f", fourteenPercentDiscount);
 			System.out.print(". (14% of your purchase)");
 			System.out.println(" ");
-		}
+		} // spending more than 210
 		
 		System.out.println(" ");
 		
